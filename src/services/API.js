54426e4ad21448d8335f15ccc3c1b4d2b192afd6
Saga <sagaswahn@hotmail.com>
2021-09-16 22:18:49 +0200

    Changed how query parameters was received in data fetch

--- src/services/API.js
+++ src/services/API.js
@@ -72,7 +72,7 @@ export const getMoviesInGenre = async ({ id, page }) => {
 };
 
 // Get search result and paginate it
-export const getSearchResult = async ({ query, page }) => {
+export const getSearchResult = async (query, page) => {
   return await getWithATR(
     `/search/multi/${apiKey}&query=${query}&page=${page}`
   );
