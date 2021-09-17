5e676e17707371add5cda726507f5a55857aeb3f
Saga <sagaswahn@hotmail.com>
2021-09-17 10:56:01 +0200

    Used body scroll lock to prevent user from scrolling when menu is open

--- src/components/partials/NavbarMobile.jsx
+++ src/components/partials/NavbarMobile.jsx
@@ -1,24 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { Link, useHistory, useLocation } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import { disableBodyScroll, clearAllBodyScrollLocks } from "body-scroll-lock";
+import { Link, useLocation } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import styles from "../css/Navbar.module.css";
-import useScrollBlock from "../../hooks/useScrollBlock";
 
 function NavbarMobile() {
   const location = useLocation();
   const [toggleMenu, setToggleMenu] = useState(false);
   const [navStyle, setNavStyle] = useState(false);
-  const [blockScroll, allowScroll] = useScrollBlock();
+  const dropdown = useRef();
 
   const navbarPosition = {
-    // When menu is open, position it absolute
+    // When menu is open, position it absolute to header
     position: "absolute",
   };
   const navbarBackground = {
     // When menu is open, give it a background color
     height: "100vh",
-    background: "#181818;",
+    background: "#181818",
   };
+
+  useEffect(() => {
+    // If menu is open, prevent body scroll w/ exception for on the dropdown menu.
+    if (toggleMenu) {
+      disableBodyScroll(dropdown);
+    } else {
+      clearAllBodyScrollLocks();
+    }
+  }, [toggleMenu]);
 
   useEffect(() => {
     if (location.pathname === "/") {
@@ -33,7 +42,11 @@ function NavbarMobile() {
   };
 
   const menu = (
-    <ul className={styles.mobileMenu} onClick={() => toggleOnClick()}>
+    <ul
+      className={styles.mobileMenu}
+      onClick={() => toggleOnClick()}
+      ref={dropdown}
+    >
       <div className={styles.links}>
         <Link to="/">Home</Link>
         <Link to="/trending">Trending</Link>
