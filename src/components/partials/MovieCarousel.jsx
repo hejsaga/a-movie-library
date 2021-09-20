2a6d9e4c3f83c8efd6899b14e86661ac5388f80e
Saga <sagaswahn@hotmail.com>
2021-09-20 10:30:51 +0200

    Added config for responsiveness

--- src/components/partials/MovieCarousel.jsx
+++ src/components/partials/MovieCarousel.jsx
@@ -4,22 +4,19 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import styles from "../css/Carousel.module.css";
 
 function ReactCarousel({ movies }) {
   const imgPrefix = "https://image.tmdb.org/t/p/w300";
 
-  useEffect(() => {
+  /* useEffect(() => {
     console.log("Movies in carousel", movies);
-  }, [movies]);
+  }, [movies]); */
 
   var settings = {
     dots: true,
-    infinite: true,
     autoplay: false,
     speed: 300,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    draggable: true,
 
     responsive: [
       {
@@ -28,7 +25,8 @@ function ReactCarousel({ movies }) {
           slidesToShow: 5,
           slidesToScroll: 4,
           infinite: true,
-          dots: true,
+          dots: false,
+          arrows: true,
         },
       },
       {
@@ -36,19 +34,30 @@ function ReactCarousel({ movies }) {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-          infinite: true,
+          infinite: false,
           dots: true,
           arrows: false,
         },
       },
       {
-        breakpoint: 770,
+        breakpoint: 1448,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: false,
+          dots: false,
           arrows: false,
+        },
+      },
+      {
+        breakpoint: 860,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: false,
+          dots: false,
+          arrows: false,
+          draggable: true,
         },
       },
     ],
@@ -60,7 +69,7 @@ function ReactCarousel({ movies }) {
         <Slider {...settings}>
           {movies.map((movie, i) => {
             return (
-              <div key={i}>
+              <div key={i} className={styles.carouselContainer}>
                 <img src={imgPrefix + movie.poster_path} alt="No image"></img>
               </div>
             );
